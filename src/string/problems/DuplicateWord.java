4de64970a0359608075403ae3663c6b12c82e873
RaiyanSolaiman <42686590+RaiyanSolaiman@users.noreply.github.com>
2018-09-10 12:49:45 -0500

    Update DuplicateWord.java

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -2,17 +2,23 @@ package string.problems;
 
 /**
  * Created by mrahman on 04/22/17.
+ * completed by Raiyan Solaiman
  */
-public class DuplicateWord {
+import java.util.*;
 
+public class DuplicateWord
+{
+    
     public static void main(String[] args) {
-        /*
-         * Write a java program to find the duplicate words and their number of occurrences in the string.
-         * Also Find the average length of the words.
-         */
 
-        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        String text = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
+        List<String> list = Arrays.asList(text.split(" "));
+
+        Set<String> uniqueWords = new HashSet<String>(list);
+        for (String word : uniqueWords) {
+            System.out.println(word + ": " + Collections.frequency(list, word));
+        }
     }
 
 }
