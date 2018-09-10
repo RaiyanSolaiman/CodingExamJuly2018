740fe4086f388db98b5e6f066405febbbd190d49
RaiyanSolaiman <42686590+RaiyanSolaiman@users.noreply.github.com>
2018-09-10 13:35:47 -0500

    Update CollectionView.java

--- src/datastructure/CollectionView.java
+++ src/datastructure/CollectionView.java
@@ -1,23 +1,33 @@
 package datastructure;
 
-import java.util.Collection;
-import java.util.HashMap;
+import java.util.*;
 
-public class CollectionView {
+public class CollectionView
+{
 
-public static void main(String[] args) {
+public static void main(String[] args)
+    {
         /*
          Map is created and inserted some data.Retrieve the Collection view of values present in map
          */
-        HashMap<Integer, String> map = new HashMap<Integer, String>();
-        map.put(1, "NYC");
-        map.put(2, "LA");
-        map.put(3, "Denver");
-        map.put(4, "Boston");
-        map.put(5, "San Jose");
-        map.put(5, "Seattle");
+    Map<String, String> map = new HashMap<>();
+    map.put("1", "NYC");
+    map.put("2", "LA");
+    map.put("3", "Denver");
+    map.put("4", "Boston");
+    map.put("5", "San Jose");
+    map.put("5", "Seattle");
 
+    Set<String> setCode = map.keySet();
+    Iterator<String> iterator = setCode.iterator();
+
+    while (iterator.hasNext())
+        {
+        String code = iterator.next();
+        String country = map.get(code);
+
+        System.out.println(code + " => " + country);
+        }
 
     }
-
 }
