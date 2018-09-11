d6bae4a4d43fe62538f30f060e11853c2134915c
RaiyanSolaiman <42686590+RaiyanSolaiman@users.noreply.github.com>
2018-09-11 14:35:11 -0500

    Update UseArrayList.java

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,16 +1,53 @@
 package datastructure;
+import java.util.*;
+public class UseArrayList
+{
 
-public class UseArrayList {
+	public static void main(String[] args)
+	{
 
-	public static void main(String[] args) {
-		/*
-		 * Demonstrate how to use ArrayList that includes add,peek,remove,retrieve elements.
-		 * Use For Each loop and while loop with Iterator to retrieve data.
-		 * Store all the sorted data into one of the databases.
-		 * 
-		 */
-	
+			/* Creating ArrayList of type "String" which means
+			 * we can only add "String" elements
+			 */
+			ArrayList<String> obj = new ArrayList<String>();
 
+			/*This is how we add elements to an ArrayList*/
+			obj.add("Ceasar");
+			obj.add("Ptomley");
+			obj.add("Cleopatra");
+			obj.add("Henry");
+			obj.add("Smeagul");
+			obj.add("Rasputin");
+
+			// Displaying elements
+			System.out.println("Original ArrayList:");
+			for(String str:obj)
+				System.out.println(str);
+
+
+			obj.add(0, "Ferdinand");
+			obj.add(1, "Adolf");
+
+
+			System.out.println("ArrayList after add operation:");
+			for(String str:obj)
+				System.out.println(str);
+
+			//Remove elements from ArrayList like this
+			obj.remove("Henry");
+			obj.remove("Smeagul");
+
+			// Displaying elements
+			System.out.println("ArrayList after remove operation:");
+			for(String str:obj)
+				System.out.println(str);
+
+
+			obj.remove(2);
+
+
+			System.out.println("Final ArrayList:");
+			for(String str:obj)
+				System.out.println(str);
+		}
 	}
-
-}
