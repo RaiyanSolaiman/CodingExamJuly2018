15640b7aa69d00b5ddd52e899d76d9c8374f12b9
RaiyanSolaiman <42686590+RaiyanSolaiman@users.noreply.github.com>
2018-09-11 15:58:46 -0500

    Update Employee.java

--- src/design/Employee.java
+++ src/design/Employee.java
@@ -1,5 +1,5 @@
 package design;
-
+import java.io.Serializable;
 public interface Employee {
 	
 	/*Employee is an Interface which contains multiple unimplemented methods.Again few methods
@@ -10,6 +10,60 @@ public interface Employee {
 	
 	//employeeId() will return employee id.
 	public int employeeId();
+
+
+
+	public class Employee implements Serializable{
+		public String empName;
+		public String empAddress;
+		public transient String empDOB;
+		public int empID;
+
+		public Employee(){}
+		public Employee(String empName, String empAddress, String empDOB, int empID) {
+			this.empName = empName;
+			this.empAddress = empAddress;
+			this.empDOB = empDOB;
+			this.empID = empID;
+		}
+
+		public String getEmpName() {
+			return empName;
+		}
+
+		public void setEmpName(String empName) {
+			this.empName = empName;
+		}
+
+		public String getEmpAddress() {
+			return empAddress;
+		}
+
+		public void setEmpAddress(String empAddress) {
+			this.empAddress = empAddress;
+		}
+
+		public String getEmpDOB() {
+			return empDOB;
+		}
+
+		public void setEmpDOB(String empDOB) {
+			this.empDOB = empDOB;
+		}
+
+		public int getEmpID() {
+			return empID;
+		}
+
+		public void setEmpID(int empID) {
+			this.empID = empID;
+		}
+
+
+
+
+
+	}
 	
 	//employeeName() will return employee name
 	public String employeeName();
